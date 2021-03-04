f18acc39a8cf991925811769b9c1137c2bef6270
cboucheIGN <clement.bouche@ign.fr>
2021-03-04 08:57:06 +0100

    dev: cours javascript, et nodejs

--- api/route/apiRoute.js
+++ api/route/apiRoute.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const medalController = require('../controller/medalController');
+const databaseController = require('../controller/databaseController');
 
 module.exports = function(app) {
 
@@ -10,4 +11,14 @@ module.exports = function(app) {
   app.route('/api/medal/search')
     .post(medalController.search);
 
+  app.route('/api/bdd/:table')
+    .get(databaseController.list);
+
 };
+
+
+
+
+
+
+//
--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,67 @@
-alert('Script map.js chargé');
+// alert('Script map.js chargé');
 
 window.onload = function () {
 
-  fetch('/api/medal/1')
+  // fetch('/api/medal/1')
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     console.log('réponse', json);
+  //     const result = document.getElementById('result');
+  //     result.innerHTML = `
+  //       <ul class="cthg-map-list">
+  //         <li>Identifiant: ${json.id}</li>
+  //         <li>Nom: ${json.athlete.name}</li>
+  //         <li>Sport: ${json.epreuve.name}</li>
+  //       </ul>
+  //     `
+  //   });
+
+  fetch('/api/bdd/livres')
     .then((response) => response.json())
     .then((json) => {
-      console.log('réponse', json);
+      console.log('réponse', json.features);
       const result = document.getElementById('result');
-      result.innerHTML = `
-        <ul class="cthg-map-list">
-          <li>Identifiant: ${json.id}</li>
-          <li>Nom: ${json.athlete.name}</li>
-          <li>Sport: ${json.epreuve.name}</li>
-        </ul>
-      `
+      var output = '<ul>';
+      for (var i = 0; i<json.features.length; i++){
+        output += '<li>'+ json.features[i].propertie.name + ' : <em>' + json.features[i].propertie.auteur + '</em></li>'
+      }
+      output += '</ul>';
+      result.innerHTML = output;
     });
+
+    var formu = document.getElementById('formu');
+    formu.addEventListener('submit', valide);
+
+    function valide(e){
+      e.preventDefault();
+      var saisie = formu.elements["ville"].value;
+      url = 'http://api-adresse.data.gouv.fr/search/?q=' + saisie;
+      // console.log(url);
+
+      fetch(url)
+        .then(r => r.json())
+        .then(r2 => {
+          console.log(r2.features[0].geometry.coordinates)
+          //TO Clément : insert code leaflet
+        });
+
+      // fetch(url)
+      //   .then(r => r.json())
+      //   .then(rep2 => {
+      //     console.log(rep2);
+      //   })
+
+    }
+
+    // fetch('http://api-adresse.data.gouv.fr/search/?q=' + 'Paris')
+    //   .then()
+
+
+
+
+
+
+
+
+
 }
