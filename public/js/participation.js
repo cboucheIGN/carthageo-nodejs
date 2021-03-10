afdb8f89f5d7d78c735a7eb7a764819108eddcea
Haltina <francois261093@gmail.com>
2021-03-10 09:23:52 +0100

    10 mars, commit du matin

--- public/js/participation.js
+++ public/js/participation.js
@@ -1,7 +1,8 @@
 // alert('Script exoleaflet.js est bien chargé!!!, Merci de votre dévéloppement. '
 // setview = latitude, longitude, niv.zoom
-var mymap = L.map('carte').setView([48, 2], 2);
+var carte = L.map('carte').setView([48, 2], 2);
 var mapboxAccessToken = 'pk.eyJ1IjoidGFraWtvdiIsImEiOiJja2x1cTk0Y3AwOWhmMm9wbHk1cGlwbmdhIn0.4IZ_FJBkcBndgp2fdCWaIQ';
+
 
 // importation des cartes leaflet(on doit changer notre propre Token créé sur mapbox)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -12,32 +13,16 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     // mon propre token // mapbox//username : Takikov
     accessToken:mapboxAccessToken
-}).addTo(mymap);
+}).addTo(carte);
 
+var wmsLayer = L.tileLayer.wms(
+  'http://localhost:8080/geoserver/web/wms',
+  {
+    layers: '1880'
+  }
+).addTo(carte);
 
 geojson = L.geoJson(statesData, {
     style: style,
     onEachFeature: onEachFeature
-}).addTo(map);
-
-// Données geoserver via postgreGIS
-var wmsLayer = L.tileLayer.wms(
-    'http://localhost:8080/geoserver/infrastructure/wms?',
-    {
-        layers: 'countries'
-    }
-).addTo(geomap);
-
-// construction de chemin de url
-const url = "http://localhost:8080/geoserver/infrastructure/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=infrastructure%3Acountries&maxFeatures=50&outputFormat=application%2Fjson"
-
-
-//importation depuis fiche xml
-fetch(url)
-    .then((response)=> response.json())
-    .then((json) => {
-        console.log(json);
-        L.geoJson(json, {
-            style: newStyle
-        }).addTo(geomap);
-    });
+}).addTo(carte);
