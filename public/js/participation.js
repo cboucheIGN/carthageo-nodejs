7aa89b9bec5ae74ca09e49de9b7fcb8d9a8f675a
Haltina <francois261093@gmail.com>
2021-03-10 11:47:22 +0100

    carte en js de 1880 essai par encore de compilation

--- public/js/participation.js
+++ public/js/participation.js
@@ -1,28 +1,17 @@
 // alert('Script exoleaflet.js est bien chargé!!!, Merci de votre dévéloppement. '
 // setview = latitude, longitude, niv.zoom
-var carte = L.map('carte').setView([48, 2], 2);
 var mapboxAccessToken = 'pk.eyJ1IjoidGFraWtvdiIsImEiOiJja2x1cTk0Y3AwOWhmMm9wbHk1cGlwbmdhIn0.4IZ_FJBkcBndgp2fdCWaIQ';
+var carte = L.map('carte').setView([20, 0], 2);
 
-
-// importation des cartes leaflet(on doit changer notre propre Token créé sur mapbox)
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+    id: 'mapbox/light-v9',
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
     tileSize: 512,
-    zoomOffset: -1,
-    // mon propre token // mapbox//username : Takikov
-    accessToken:mapboxAccessToken
+    zoomOffset: -1
 }).addTo(carte);
 
-var wmsLayer = L.tileLayer.wms(
-  'http://localhost:8080/geoserver/web/wms',
-  {
-    layers: '1880'
-  }
-).addTo(carte);
+L.geoJson(carte1880).addTo(carte);
 
-geojson = L.geoJson(statesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(carte);
+
+// geojson = L.geoJson(wmsLayer, {
+// }).addTo(carte);
