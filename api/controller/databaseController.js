e874c672cad48b3e9143919475292075b2d55173
TAKIKV <hamou22221@gmail.com>
2021-03-11 15:48:38 +0100

    recup date2

--- api/controller/databaseController.js
+++ api/controller/databaseController.js
@@ -3,8 +3,7 @@
 const config = require('../../config');
 const pgp = require('pg-promise')({});
 const db = pgp(config.database_olympics);
-const date = 1896
-
+const date = 1932;
 
 exports.list = function (req, res){
   const table = req.params.table;
@@ -15,6 +14,33 @@ exports.list = function (req, res){
     console.log(req.params);
     return premiereparticipation(req, res);
   }
+}
+
+exports.listcountry = function (req, res){
+  console.log(req.body.c)
+  let sql = "SELECT * FROM country WHERE (first_participation <= " + req.body.c + " AND last_participation >= " + req.body.c + ") ORDER BY name;";
+  console.log(sql)
+    db.any(sql)
+    .then((data) => {
+      const result = data.map((item) => {
+        return {
+          type : 'feature',
+          propertie : {
+            id: item.id,
+            name: item.name,
+            first_participation: item.first_participation,
+            last_participation: item.last_participation
+          }
+        }
+      });
+      res.json({
+        type:'featureatureCollection',
+        features: result
+      });
+    })
+    .catch((error) => {
+      res.send(error);
+    })
 }
 
 const premiereparticipation = function(req, res){
@@ -54,7 +80,46 @@ const premiereparticipation = function(req, res){
 
 
 
+
+
 // import des données de medaller pour création de table/
 
+exports.medal_track = function (req, res){
+  const table = req.params.table;
+  console.log(req.params)
+  // // checking error Function
+  //check(req, res);
+  if(table == 'medaller'){
+    console.log(req.params);
+    return medaller(req, res);
+  }
+}
 
+const medaller = function(req, res){
+  console.log(req.params)
+  let sql = "SELECT * FROM medaller_country";
+  console.log(sql)
+    db.any(sql)
+    .then((data) => {
+      const result = data.map((item) => {
+        return {
+          type : 'feature',
+          propertie : {
+            olympiad: item.olympiad_id,
+            country_id: item.country_id,
+            country: item.name,
+            gold: item.gold,
+            silver: item.silver,
+            bronze: item.bronze,
+          }
+        }
+      });
+      res.json({
+        type:'featureatureCollection',
+        features: result
+      });
+    })
+    .catch((error) => {
+      res.send(error);
+    })
 }
