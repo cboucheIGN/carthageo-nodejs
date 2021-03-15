8a615bbf98f0fc754101d96dac87a6e97a13439b
Haltina <francois261093@gmail.com>
2021-03-15 16:48:44 +0100

    listing olympiad

--- public/js/participation.js
+++ public/js/participation.js
@@ -19,13 +19,16 @@ var listpays = [];
 var LayerGroup = new L.LayerGroup();
 var bool = true;
 var PaysJSON;
-form.elements["valuedate"].value = 1896
 
 // fonction qui va se déclancher au moment de la séléction de la date
 function roulant(e){
+  reponse = form.elements["valuedate"].value;
+  datechoisi = reponse[0] + reponse[1] + reponse[2] + reponse[3];
+  console.log(reponse);
+  console.log(datechoisi);
   var listpays = [];
   console.log(form.elements["valuedate"].value);
-  var datechoisi = form.elements["valuedate"].value;
+  // var datechoisi = form.elements["valuedate"].value;
   var choix = {c:datechoisi};
 
   // va afficher les données qu'il va chercher dans la base de données olympics
@@ -70,7 +73,7 @@ function onEachFeature(feature, layer) {
     });
 }
 
-function getColor(first, last) {
+function getColor(first, last,d) {
   d = last - first;
     return d <= 4 ? '#ffffcc' :
            d <= 16 ? '#c2e699' :
@@ -79,6 +82,8 @@ function getColor(first, last) {
           '#006837';
 }
 function highlightFeature(e) {
+    reponse = form.elements["valuedate"].value;
+    datechoisi = reponse[0] + reponse[1] + reponse[2] + reponse[3];
     var layer = e.target;
     layer.setStyle({
         weight: 5,
@@ -88,26 +93,68 @@ function highlightFeature(e) {
     });
 }
 
+function addDate(){
+  var formedate = document.getElementById('formdate').elements["valuedate"];
+  var date = [];
+  fetch('/api/bdd/searchdate',{
+    method : 'post',
+    body: JSON.stringify({ c: "9"}),
+    headers: {'Content-Type': 'application/json'}
+  })
+    .then(req => req.json())
+    .then((req) => {
+      console.log(req);
+      for (var i =0; i < req.features.length;i++){
+        date += req.features[i].propertie.year;
+        var option = document.createElement('option');
+        option.innerHTML = req.features[i].propertie.year + ", " + req.features[i].propertie.city + ", " + req.features[i].propertie.country + ", " + req.features[i].propertie.season + "</br>";
+        formedate.appendChild(option);
+      }
+    console.log(date);
+  })
+}
+addDate();
+
 function resetHighlight(e) {
   geojson.resetStyle(e.target);
 }
 
 function style(feature) {
+  reponse = form.elements["valuedate"].value;
+  datechoisi = reponse[0] + reponse[1] + reponse[2] + reponse[3];
     return {
-        fillColor: getColor(feature.properties.first_participation, form.elements["valuedate"].value),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
+      fillColor: getColor(feature.properties.first_participation, datechoisi),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
     };
 }
 
 var popup = L.popup()
 function displayInfos(event) {
+  reponse = form.elements["valuedate"].value;
+  datechoisi = reponse[0] + reponse[1] + reponse[2] + reponse[3];
   console.log(event);
   console.log("Tu as cliqué sur le pays : " + event.target.feature.properties.name + " qui a participé pour la première fois en " + event.target.feature.properties.first_participation + " et a participé pour la dernière fois en " + event.target.feature.properties.last_participation);
   popup
     .setLatLng(event.latlng)
-    .setContent("<br>Pays : " + event.target.feature.properties.name + "</br><br>Première participation : " + event.target.feature.properties.first_participation + "</br><br>Dernière participation : " + event.target.feature.properties.last_participation + "</br><br>Durée : " + (event.target.feature.properties.last_participation - event.target.feature.properties.first_participation) + " années</br>" + "<br>En " + form.elements["valuedate"].value + "</br><br>Durée : " + (form.elements["valuedate"].value - event.target.feature.properties.first_participation) + " ans </br>").openOn(carte);
+    .setContent("<p id=\"popup\"><br>Pays : " + event.target.feature.properties.name + "</br><br>Première participation : " + event.target.feature.properties.first_participation + "</br><br>Dernière participation : " + event.target.feature.properties.last_participation + "</br><br>Durée : " + (event.target.feature.properties.last_participation - event.target.feature.properties.first_participation) + " années</br>" + "<br>En " + datechoisi + "</br><br>Durée : " + (datechoisi - event.target.feature.properties.first_participation) + " ans </br></p>").openOn(carte);
 }
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (carte) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [4,16,32,64],
+        labels = []
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor() + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+    return div;
+};
+legend.addTo(carte);
